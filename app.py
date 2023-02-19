64e453e4d295f8eb4866b5e916c20eed27cc12e7
unknown <jalil.zoabi@gmail.com>
2023-02-20 01:02:58 +0200

    3

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask_cors import CORS
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
+# live back-end server: https://one90223-web1-project.onrender.com/students
+
 
 app = Flask(__name__)
 CORS(app)
